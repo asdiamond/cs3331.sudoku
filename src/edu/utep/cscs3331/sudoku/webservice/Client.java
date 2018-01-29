4aede8587943aa6f28158dd4cc4a1525c1810ad3
Aleksandr <asdiamond@miners.utep.edu>
2018-01-28 23:24:10 -0700

    Added some links for web service.

---
+++ src/edu/utep/cscs3331/sudoku/webservice/Client.java
@@ -0,0 +1,11 @@
+package edu.utep.cscs3331.sudoku.webservice;
+
+/**
+ * Author: Aleksandr Diamond
+ * Assignment: sudoku
+ * Purpose:
+ */
+//http://www.studytrails.com/java/json/java-google-json-parse-json-to-java/
+//http://www.cs.utep.edu/cheon/ws/sudoku/
+public class Client {
+}
