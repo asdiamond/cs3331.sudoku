8cb1de7d0b7196ddf661764725d831eed1050fad
Aleksandr <asdiamond@miners.utep.edu>
2018-03-19 20:57:17 -0600

    Added test template

---
+++ src/tests/edu/utep/cscs3331/sudoku/graphics2d/model/BoardTest.java
@@ -0,0 +1,53 @@
+package edu.utep.cscs3331.sudoku.graphics2d.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Author: Aleksandr Diamond on 3/19/2018
+ * Assignment: sudoku
+ * Purpose:
+ */
+class BoardTest {
+
+    @BeforeEach
+    void setUp() {
+        Board board = new Board();
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void isSolved() {
+    }
+
+    @Test
+    void getSquare() {
+    }
+
+    @Test
+    void updateBoard() {
+    }
+
+    @Test
+    void isValidMove() {
+    }
+
+    @Test
+    void getIndex() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void getSelectedSquare() {
+    }
+}
---
+++ src/tests/edu/utep/cscs3331/sudoku/graphics2d/model/SquareTest.java
@@ -0,0 +1,47 @@
+package edu.utep.cscs3331.sudoku.graphics2d.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Author: Aleksandr Diamond on 3/19/2018
+ * Assignment: sudoku
+ * Purpose:
+ */
+class SquareTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getRow() {
+    }
+
+    @Test
+    void getCol() {
+    }
+
+    @Test
+    void getVal() {
+    }
+
+    @Test
+    void setVal() {
+    }
+
+    @Test
+    void isSelected() {
+    }
+
+    @Test
+    void setSelected() {
+    }
+}
